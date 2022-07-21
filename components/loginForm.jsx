4bb59be7ae29a809b2c7828c622204e69005393e
Ethan Swan <eswan18@rocketmail.com>
2022-07-20 19:42:11 -0500

    Pretend to have login.

---
+++ components/loginForm.jsx
@@ -0,0 +1,9 @@
+import styles from './loginForm.module.css';
+
+export default function LoginForm() {
+  return (
+    <div className={styles.login}>
+      Here you can just type everything about yourself including your credit card details
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,20 @@
 import MainLayout from '../components/mainLayout';
+import LoginForm from '../components/loginForm';
 
 export default function Home() {
+  const loggedIn = false;
   return (
-    <MainLayout home>
+    loggedIn
+    ? <MainLayout home>
+      <div>
+        <h1>You're In!</h1>
+        content goes here
+      </div>
+    </MainLayout>
+    : <MainLayout home>
       <div>
         <h1>Welcome</h1>
-        { /* <LoginForm/> */ }
+        <LoginForm/>
       </div>
     </MainLayout>
   );
