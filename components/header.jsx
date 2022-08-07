df4cde1dd42e4c06331204cfcfdf22d1754d6a5c
Ethan Swan <eswan18@rocketmail.com>
2022-08-07 07:42:10 -0500

    Add ability to log out.

--- components/header.jsx
+++ components/header.jsx
@@ -1,23 +1,14 @@
 import { useEffect, useState } from 'react';
-import { getLocalUsername } from '../lib/auth';
+import HeaderLoginStatusBox from './headerLoginStatusBox';
 import styles from './header.module.css';
 
 
 export default function Header({ children, siteTitle }) {
-  const [username, setUsername] = useState(null)
-
-  useEffect(() => {
-    /* Use the username from local storage if there is one. */
-    const localUsername = getLocalUsername('ewa-username')
-    if (localUsername) {
-      setUsername(localUsername)
-    }
-  }, [])
-
   return (
     <div className={styles.header}>
+      <div className={styles.headerLeft}></div>
       { siteTitle }
-      <div className={styles.headerRight}>{username || 'unauthenticated'}</div>
+      <div className={styles.headerRight}><HeaderLoginStatusBox/></div>
     </div>
   );
 }
---
+++ components/headerLoginStatusBox.jsx
@@ -0,0 +1,33 @@
+import Router from 'next/router';
+import { useState, useEffect } from 'react';
+import { getLocalUsername, logout } from '../lib/auth';
+import styles from './headerLoginStatusBox.module.css';
+
+export default function HeaderLoginStatusBox() {
+  const [username, setUsername] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    /* Use the username from local storage if there is one. */
+    const localUsername = getLocalUsername('ewa-username')
+    if (localUsername) {
+      setUsername(localUsername)
+    }
+    setLoading(false)
+  }, [])
+
+  const logoutAndGoToLogin = () => {
+      logout()
+      Router.push('/login')
+  }
+  const content = username
+    ? (<>
+        <p>{username}</p><button onClick={logoutAndGoToLogin}>Logout</button>
+    </>)
+    : (<p>unauthenticated</p>)
+
+  return !loading && (
+    <div className={styles.box}>{content}</div>
+  )
+        
+}
