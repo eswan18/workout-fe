420c7ca62abd6cb7f4a190b045ef484921846272
Ethan Swan <eswan18@rocketmail.com>
2022-08-17 22:16:45 -0500

    Update headerLoginStatusBox for nextauth.

--- components/mainLayout/header/headerLoginStatusBox.jsx
+++ components/mainLayout/header/headerLoginStatusBox.jsx
@@ -1,33 +1,15 @@
-import Router from 'next/router';
-import { useState, useEffect } from 'react';
-import { getLocalUsername, logout } from '../../../lib/auth';
+import { useSession, signOut } from "next-auth/react"
 import styles from './headerLoginStatusBox.module.css';
 
 export default function HeaderLoginStatusBox() {
-  const [username, setUsername] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const { data: session, status } = useSession()
 
-  useEffect(() => {
-    /* Use the username from local storage if there is one. */
-    const localUsername = getLocalUsername()
-    if (localUsername) {
-      setUsername(localUsername)
-    }
-    setLoading(false)
-  }, [])
-
-  const logoutAndGoToLogin = () => {
-      logout()
-      Router.push('/login')
-  }
-  const content = username
+  const content = status === "authenticated"
     ? (<>
-        <p>{username}</p><button onClick={logoutAndGoToLogin}>Logout</button>
+        <p>{session.user.email}</p><button onClick={signOut}>Logout</button>
     </>)
     : (<p>Unauthenticated</p>)
 
-  return !loading && (
-    <div className={styles.box}>{content}</div>
-  )
+  return <div className={styles.box}>{content}</div>
         
 }
