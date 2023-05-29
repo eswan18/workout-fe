8e62ab8568e00a8ce90d21cc4c902936f1a0f096
Ethan Swan <eswan18@rocketmail.com>
2023-05-28 21:39:33 -0500

    Clean up landing page.

---
+++ app/create-account/page.tsx
@@ -0,0 +1,10 @@
+export default function CreateAccountPage() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+            { /* A white div with rounded corners and a border */ }
+            <h1>Welcome to da club</h1>
+          </div>
+      </main>
+    )   
+  }
