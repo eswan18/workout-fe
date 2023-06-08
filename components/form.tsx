a47183e63556a334fcfa92c4da13d7e5b8195a24
Ethan Swan <ethanpswan@gmail.com>
2023-06-08 08:12:50 -0400

    Touch up more form stuff.

--- components/form.tsx
+++ components/form.tsx
@@ -9,9 +9,9 @@ export default function Form({ title, action, className, children }: FormProps) 
   return (
     <form
         action={action}
-        className={`flex flex-col text-base p-2 md:p-4 rounded-md border-2 border-gray-700 dark:border-gray-100 shadow-md shadow-neutral-400 dark:shadow-neutral-200 ${className}`}
+        className={`flex flex-col ${className}`}
     >
-      <h2 className="text-2xl font-bold py-3 mt-2">{title}</h2>
+      <h2 className="text-2xl font-bold my-2">{title}</h2>
       { children }
     </form>
   )
