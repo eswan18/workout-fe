6ae346ef7026553b36a95c2a46c083162356817c
Ethan Swan <ethanpswan@gmail.com>
2023-08-20 21:16:55 -0500

    Remove property that doesn't exist.

--- components/forms/Form.tsx
+++ components/forms/Form.tsx
@@ -2,18 +2,16 @@ interface FormProps {
   title: string;
   action?: (data: FormData) => any;
   onSubmit?: (event: React.FormEvent<HTMLFormElement>) => void;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
   children: React.ReactNode;
 }
 
-export default function Form({ title, action, onSubmit, onChange, className, children }: FormProps) {
+export default function Form({ title, action, onSubmit, className, children }: FormProps) {
   return (
     <div className="flex flex-col items-center">
       <form
           action={action}
           onSubmit={onSubmit}
-          onChange={onChange}
           className={`flex flex-col ${className}`}
       >
         <h2 className="text-2xl font-bold my-2">{title}</h2>
