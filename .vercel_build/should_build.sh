f150ffc7d5c79cf1633377cad7fa85fcabbd4abf
Ethan Swan <ethanpswan@gmail.com>
2023-08-25 17:36:34 -0500

    Add comment.

--- .vercel_build/should_build.sh
+++ .vercel_build/should_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # This script tells Vercel to only build the staging and main branches.
+# I copied it from here: https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel#with-environment-variables
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
