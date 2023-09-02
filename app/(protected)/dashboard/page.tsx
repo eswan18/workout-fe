5be1f16e7f7bcfdde976ed3566cc3d77dfe407cd
Ethan Swan <ethanpswan@gmail.com>
2023-09-02 11:27:44 -0500

    Simplify dashboard page.

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,16 +1,6 @@
-import { getAccessToken } from "@/lib/session";
 import { getAllWorkoutTypes } from "@/lib/resources/workoutTypes/getWorkoutTypes";
-import { WorkoutType } from "@/lib/resources/apiTypes";
+import { getAllWorkouts } from "@/lib/resources/workouts/getWorkouts";
 import NewWorkoutPanel from "./NewWorkoutPanel";
-
-
-const apiUrl = process.env.WORKOUT_API_URL;
-
-class WorkoutMetrics {
-  constructor(
-    public workoutCount: number,
-  ) {}
-} 
 
 function WelcomeBanner({ workoutCount }: { workoutCount: number }) {
   return (
@@ -21,35 +11,14 @@ function WelcomeBanner({ workoutCount }: { workoutCount: number }) {
   )
 }
 
-async function getMetricsData(): Promise<WorkoutMetrics> {
-  const token = await getAccessToken();
-
-  // Request /workouts and pass the access token.
-  const response = await fetch(`${apiUrl}/workouts`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  const workouts = await response.json();
-  const metrics = new WorkoutMetrics(workouts.length);
-  
-  return metrics;
-}
-
 export default async function DashboardPage() {
-  const metrics = await getMetricsData();
-  let workoutTypes: WorkoutType[] | null = null;
-  try {
-    workoutTypes = await getAllWorkoutTypes();
-  } catch (error) {
-    console.error(error);
-  }
+  const workouts = await getAllWorkouts();
+  const workoutTypes = await getAllWorkoutTypes();
 
   return (
     <main className="flex min-h-screen flex-col items-start p-10 lg:p-16">
       <div className="mb-4 lg:mb-12">
-        <WelcomeBanner workoutCount={metrics.workoutCount}/>
+        <WelcomeBanner workoutCount={workouts.length}/>
       </div>
       <div className="w-full max-w-5xl items-center justify-around lg:flex">
         <NewWorkoutPanel workoutTypes={workoutTypes} error='Failed to fetch workout type data' />
--- lib/resources/workouts/getWorkouts.ts
+++ lib/resources/workouts/getWorkouts.ts
@@ -3,10 +3,16 @@
 import { get } from '@/lib/requests';
 import { Workout } from '@/lib/resources/apiTypes';
 
+const ROUTE = `/workouts/`;
+
 export async function getWorkoutById(id: string): Promise<Workout> {
-  const workouts = await get({route: `/workouts/`, params: {id}}) as Workout[];
+  const workouts = await get({route: ROUTE, params: {id}}) as Workout[];
   if (workouts.length > 1) {
     throw new Error(`Multiple workouts found with id ${id}`);
   }
   return workouts[0];
+}
+
+export async function getAllWorkouts(): Promise<Workout[]> {
+  return await get({route: ROUTE}) as Workout[];
 }
