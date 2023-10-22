03839e0936b15629965745660533e0b8a9f51664
Ethan Swan <ethanpswan@gmail.com>
2023-10-22 16:13:37 -0700

    Update storybook for recent changes.

--- stories/Components/NavBar/NavBar.stories.tsx
+++ stories/Components/NavBar/NavBar.stories.tsx
@@ -1,10 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { SyncNavBar } from "@/components/navBar/navBar";
+import NavBar from "@/components/navBar/navBar";
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
   title: "Components/NavBar/NavBar",
-  component: SyncNavBar,
+  component: NavBar,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
@@ -12,7 +12,7 @@ const meta = {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ["autodocs"],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} satisfies Meta<typeof SyncNavBar>;
+} satisfies Meta<typeof NavBar>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
