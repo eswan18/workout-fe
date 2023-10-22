bb23b5fcd04888ae6ae47601f27e5f3c191203a5
Ethan Swan <ethanpswan@gmail.com>
2023-10-22 16:24:16 -0700

    Re-add gold as a color.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        gold: "#CA8A04",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
