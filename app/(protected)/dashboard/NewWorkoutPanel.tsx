d3afd73911b171d55808482e15be959d2bc19155
Ethan Swan <ethanpswan@gmail.com>
2023-10-22 15:52:53 -0700

    Replace NewWorkoutTypes modal.

--- app/(protected)/dashboard/NewWorkoutPanel.tsx
+++ app/(protected)/dashboard/NewWorkoutPanel.tsx
@@ -3,11 +3,17 @@
 import { Workout, WorkoutType } from "@/lib/resources/apiTypes";
 
 import { createWorkout } from "@/lib/resources/workouts";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
-import MoreWorkoutTypesModal from "./MoreWorkoutTypesModal";
+import { MoreHorizontal, Weight } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { ArrowRight, Goal, MoreHorizontal, MoreHorizontalIcon, MoveRight, Weight } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
 
 async function createAndStartWorkout(
   workoutTypeId: string | undefined,
@@ -30,7 +36,6 @@ export default function NewWorkoutPanel({
 }: {
   workoutTypes: WorkoutType[];
 }) {
-  const [modal, setModal] = useState<React.ReactNode | null>(null);
   const router = useRouter();
   // The parent component should pass workout types in order of display priority
   // (descending), so we can just pull the first few.
@@ -45,23 +50,13 @@ export default function NewWorkoutPanel({
       <NewWorkoutButton name={workoutType.name} onClick={onClick} key={index} />
     );
   });
-  const onMoreClick = () => {
-    setModal(
-      <MoreWorkoutTypesModal
-        workoutTypes={workoutTypes}
-        handleClose={() => setModal(null)}
-        createAndStartWorkout={createAndStartWorkout}
-      />,
-    );
-  };
   return (
     <div className="w-full pt-2">
       {workoutTypes && <h2 className="text-lg lg:text-2xl">New Workout</h2>}
       <div className="flex flex-row flex-wrap mt-2 gap-2 lg:gap-4 justify-center lg:justify-start items-center">
         {newWorkoutCards}
-        <MoreWorkoutsButton onClick={onMoreClick} />
+        <MoreWorkoutTypesButtonWithDropdown workoutTypes={workoutTypes} />
       </div>
-      {modal}
     </div>
   );
 }
@@ -75,30 +70,44 @@ function NewWorkoutButton({
   name,
   onClick,
 }: NewWorkoutButtonProps) {
-    /*<Link href={`/workouts/${workout.id}`}>
-      <Card className="w-40 h-40 flex flex-col justify-between">
-        <CardHeader className="p-4">
-          <CardDescription>{startTimeText}</CardDescription>
-          <CardTitle className="text-lg">{name}</CardTitle>
-        </CardHeader>
-        <CardFooter className="flex flex-col items-start p-4 gap-1">
-          <p className="text-sm text-muted-foreground">{exerciseCount} sets</p>
-          <p className="text-sm text-muted-foreground">{durationString}</p>
-        </CardFooter>
-      </Card>
-    </Link> */
   return (
-    <Button variant='outline' className="w-32 h-16 flex flex-col justify-between items-center" onClick={onClick}>
-        <Weight />{name} 
+    <Button variant='ghost' className="w-auto h-16 flex flex-col justify-between items-center" onClick={onClick}>
+        <Weight />
+        <p>{name}</p>
     </Button>
   );
 }
 
-function MoreWorkoutsButton({ onClick }: { onClick: () => void }) {
+function MoreWorkoutTypesButtonWithDropdown({workoutTypes}: {workoutTypes: WorkoutType[]}) {
   return (
-    <Button variant='outline' className="w-32 h-16 lg:w-auto flex flex-col justify-between items-center" onClick={onClick}>
-      {/* Forcing the button to be exactly 32 in small screens helps because then it lines up better in two-row layouts. */}
-      <MoreHorizontal />More Options 
-    </Button>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant='ghost' className="h-16 w-auto flex flex-col justify-between items-center">
+          <MoreHorizontal />
+          <p>More workouts</p>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>All Workouts</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {
+          workoutTypes.map((wktType) => <WorkoutTypeMenuItem workoutType={wktType} key={wktType.id} />)
+        }
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
+}
+
+function WorkoutTypeMenuItem({workoutType}: {workoutType: WorkoutType}) {
+  const router = useRouter();
+  const onClick = () => {
+    createAndStartWorkout(workoutType.id).then((workout) => {
+      router.push(`/live/workouts/${workout.id}`)
+    })
+  }
+  return (
+    <DropdownMenuItem onClick={onClick} >
+      { workoutType.name }
+    </DropdownMenuItem>
+  )
 }
