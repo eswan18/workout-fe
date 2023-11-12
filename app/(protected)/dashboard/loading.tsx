75ebb65fcbd9367645f2343daf496678f46f3d20
Ethan Swan <ethanpswan@gmail.com>
2023-11-12 16:40:02 -0500

    Fix up dashboard loading page.

--- app/(protected)/dashboard/loading.tsx
+++ app/(protected)/dashboard/loading.tsx
@@ -4,15 +4,15 @@ export default function Loading() {
   return (
     <main className="flex flex-col justify-start gap-10 p-10 lg:p-16">
       <div className="mb-4">
-        <h1 className="text-2xl lg:text-4xl">Welcome!</h1>
+        <h1 className="text-4xl">Your Dashboard</h1>
       </div>
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[75%]" />
+        <Skeleton className="h-4 w-full" />
       </div>
       <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+        <Skeleton className="h-4 w-[75%]" />
+        <Skeleton className="h-4 w-full" />
       </div>
     </main>
   );
