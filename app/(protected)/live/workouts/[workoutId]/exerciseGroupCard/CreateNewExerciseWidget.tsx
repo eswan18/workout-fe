ea32952b5a9f1fe25333429a80b78aa8560255ef
Ethan Swan <ethanpswan@gmail.com>
2023-11-12 16:27:42 -0500

    Make new set button a real secondary.

--- app/(protected)/live/workouts/[workoutId]/exerciseGroupCard/CreateNewExerciseWidget.tsx
+++ app/(protected)/live/workouts/[workoutId]/exerciseGroupCard/CreateNewExerciseWidget.tsx
@@ -11,16 +11,16 @@ export default function CreateNewExerciseWidget({
 }) {
   return (
     <Card className="w-20 h-20">
-      <CardContent className="h-full flex flex-col justify-center items-center p-0">
-        <Button
-          className="text-gold p-0"
-          title="Record new set"
-          onClick={onClick}
-          variant="ghost"
-        >
-          <PlusSquare size={48} strokeWidth={1.5} />
-        </Button>
-      </CardContent>
+      <Button
+        className="w-full h-full"
+        title="Record new set"
+        onClick={onClick}
+        variant="secondary"
+      >
+        <CardContent className="h-full flex flex-col justify-center items-center p-0">
+            <PlusSquare size={48} strokeWidth={1.2} className="p-0 m-0" />
+        </CardContent>
+      </Button>
     </Card>
   );
 }
