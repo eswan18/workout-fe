d2742aa92d84fd0eef9e8cbcaa69414aaca2dec3
Ethan Swan <ethanpswan@gmail.com>
2023-11-23 21:42:24 -0600

    Finish initial layout of Exercise detail card.

--- app/(protected)/live/workouts/[workoutId]/exerciseGroupCard/ExerciseCard.tsx
+++ app/(protected)/live/workouts/[workoutId]/exerciseGroupCard/ExerciseCard.tsx
@@ -1,7 +1,7 @@
 import { SaveStatus } from "@/components/indicators/SaveStatusIndicator";
 import SaveStatusOverlayContainer from "./SaveStatusOverlayContainer";
 import { Card, CardContent } from "@/components/ui/card";
-import { X } from "lucide-react";
+import { Edit, Trash, X } from "lucide-react";
 import {
   Dialog,
   DialogTrigger,
@@ -34,25 +34,31 @@ export default function ExerciseCard({
       </DialogTrigger>
       <DialogContent className="flex flex-row justify-center">
         <div className="sm:w-64">
-          <div className="flex flex-row justify-evenly items-start">
-            <DialogHeader>
+          <DialogHeader className="flex flex-row justify-between items-start gap-4 space-y-0">
+            <div className="flex flex-col">
               <DialogTitle>{exercise.exercise_type_name}</DialogTitle>
-            </DialogHeader>
-            <span className="text-sm text-muted-foreground">
-              {exercise.start_time
-                ? formatDateYMD(exercise.start_time, true)
-                : "unknown"}
-            </span>
+              <span className="text-sm text-muted-foreground">
+                {exercise.start_time
+                  ? formatDateYMD(exercise.start_time, true)
+                  : "unknown"}
+              </span>
+            </div>
+            <div className="flex flex-row gap-1 sm:gap-3 justify-end">
+              <Button size="icon" variant="secondary" className="h-8 w-8 p-0.5"><Edit size={16} /></Button>
+              <Button size="icon" variant="destructive" className="h-8 w-8 p-0.5"><Trash size={16} /></Button>
+            </div>
+          </DialogHeader>
+          <div className="flex flex-row justify-center py-4">
+            <div className="w-fit grid grid-cols-2 py-2 gap-y-4 items-center">
+              <div className="text-base text-muted-foreground">Weight</div>
+              <div className="text-2xl text-right">{exercise.weight}</div>
+              <div className="text-base text-muted-foreground">Reps</div>
+              <div className="text-2xl text-right">{exercise.reps}</div>
+            </div>
           </div>
-          <div className="grid grid-cols-2 py-2 gap-2 items-end w-fit">
-            <div className="text-2xl text-right">{exercise.weight}</div>
-            <div className="text-lg text-muted-foreground">pounds</div>
-            <div className="text-2xl text-right">{exercise.reps}</div>
-            <div className="text-lg text-muted-foreground">reps</div>
-          </div>
-          <DialogFooter>
+          <DialogFooter className="flex flex-row justify-center sm:justify-center">
             <DialogClose asChild>
-              <Button variant="secondary">Close</Button>
+              <Button variant="outline" className="w-24">Done</Button>
             </DialogClose>
           </DialogFooter>
         </div>
